b4ffd1afa89bbbf1d6c7a127195445a23ed1ec1d
Figo Perdana Putra <57206216+figoperdana@users.noreply.github.com>
2022-04-05 16:24:28 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { createApp } from "vue";
+import { createApp, ref } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js"
+import '@/assets/main.css'
+
